72f3a0ad7ca3cf981e2983c4358e5187006c27a6
Nitya-Kalloli <nityakalloli.dev@gmail.com>
2024-08-27 20:23:48 +0530

    Fixing layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,9 @@
 // import { Sidebar } from "lucide-react";
 import Sidebar from "@/components/Sidebar";
 // used for main components of the dashboard/home
-export default function RootLayout({
+import Image from "next/image";
+import MobileNav from "@/components/MobileNav";
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
@@ -10,7 +12,15 @@ export default function RootLayout({
   return (
     <main className="flex h-screen w-full font-inter">
       <Sidebar user={ loggedIn} />
-          {children}
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
+          <div>
+            <MobileNav user={loggedIn} />
+          </div>
+        </div>
+        {children}
+      </div>
   </main>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  theme: {
+    extend: {
+      fontSize: {
+        '26': '26px',
+      },
+      colors: {
+        'black-1': '#000000', // Adjust the color value as needed
+      'black-2': '#222222',
+      },
+      fontFamily: {
+        'ibm-plex-serif': ['IBM Plex Serif', 'serif'], // Ensure you have the font available
+      },
+    },
+  },
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
